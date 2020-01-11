66b9fa21391e45789cb89e8d4cb17fbe9b54719b
Dirk Schut <dschut@outlook.com>
2020-01-11 03:57:17 +0100

    Implementing the localization system with the skills

--- js/data/skills.js
+++ js/data/skills.js
@@ -1,7 +1,54 @@
 const skillData = [
+    //Magic (0-2)
     {
-        id: 0,
-        name: "Spatial Magics",
-        levelCap: 10
+        id: skillIDS.magic,
+        lid: "magic",
+        levelCap: 10,
+    },
+    {
+        id: skillIDS.spatialmagics,
+        lid: "spatialmagics",
+        levelCap: 10,
+        parentSkill: skillIDS.magic,
+    },
+    {
+        id: skillIDS.runicmagics,
+        lid: "runicmagics",
+        levelCap: 10,
+        parentSkill: skillIDS.magic,
+    },
+
+    //Animals (3-7)
+    {
+        id: skillIDS.animalcare,
+        lid: "animalcare",
+        levelCap: 10,
+    },
+    {
+        id: skillIDS.husbandry,
+        lid: "husbandry",
+        levelCap: 10,
+        parentSkill: skillIDS.animalcare,
+    },
+    {
+        id: skillIDS.butchering,
+        lid: "butchering",
+        levelCap: 10,
+        parentSkill: skillIDS.animalcare,
     }
 ];
+
+const skillIDS = {
+    magic: 0,
+    spatialmagics: 1,
+    runicmagics: 2,
+    animalcare: 3,
+    husbandry: 4,
+    butchering: 5,
+    skinning: 6,
+    taming: 7,
+    woodcutting: 8,
+    mining: 9,
+    smelting: 10,
+    farming: 11,
+};
