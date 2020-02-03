38b416adc9257b661b04c1579859d43963eabacd
Dirk Schut <dschut@outlook.com>
2020-02-03 17:29:28 +0100

    Added item class

--- js/items.js
+++ js/items.js
@@ -1,3 +1,45 @@
+//Class which contains all the item data pertaining to the save.
+class Item{
+    constructor(id){
+        this.id = id;
+        this.amount = 0;
+    }
+
+    //Get the relevant item data.
+    getItemData(){
+        return itemData[this.id];
+    }
+
+    //Add the given amount to the item inventory.
+    addAmount(amount){
+        this.amount += amount;
+        if(this.amount > this.getItemData().cap){
+            this.amount = this.getItemData().cap;
+        }
+    }
+
+    //Checks to see if a given amount of the item can be removed from the inventory.
+    canRemoveAmount(amount){
+        if(this.amount >= amount){
+            return true;
+        }
+        return false;
+    }
+
+    //Removes the given amount of the item from the inventory if possible.
+    removeAmount(amount){
+        if(canRemoveAmount(amount)){
+            this.amount -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    get cap(){
+        return this.getItemData().cap;
+    }
+}
+
 //Displays the items in the items tab.
 function showItems(){
     console.log("---Showing Items---");
@@ -27,25 +69,16 @@ function getItemText(itemID){
 //Adds a given amount to a given item.
 function addItemToInventory(item, amount){
     if(items[item] == null){
-        items[item] = {};
-        items[item].amount = amount;
-    }else{
-        items[item].amount += amount;
+        items[item] = new Item(item);
     }
-
-    if(items[item].amount > itemData[item].cap){
-        currentAction.messages.push("Itemcap reached.");
-        items[item].amount = itemData[item].cap;
-    }
+    items[item].addAmount(amount);
     displayInventory();
 }
 
 //Checks to see if an item can be removed from the inventory. Returns true if successful and false if not.
 function canRemoveFromInventory(itemID, amount){
     if(items[itemID] != null){
-        if(items[itemID].amount >= amount){
-            return true;
-        }
+        return items[itemID].canRemoveAmount(amount);
     }
     return false;
 }
@@ -53,10 +86,7 @@ function canRemoveFromInventory(itemID, amount){
 //Removes an item from the inventory. Returns true if successful and false if not.
 function removeFromInventory(itemID, amount){
     if(items[itemID] != null){
-        if(items[itemID].amount >= amount){
-            items[itemID].amount -= amount;
-            return true;
-        }
+        return items[itemID].removeAmount(amount);
     }
     return false;
 }
@@ -66,7 +96,7 @@ function displayInventory(){
     let invText = "";
 
     for(item in items){
-        invText += "<li>" + items[item].amount + "/" + itemData[item].cap + " " + i18next.t("items." + item + ".name") + "</li>";
+        invText += "<li>" + items[item].amount + "/" + items[item].cap + " " + i18next.t("items." + item + ".name") + "</li>";
     }
 
     $("#inventory").html(invText);
